1924e3a7ad1628dffa1bf166c603b5debf3c0eba
Osmair Coelho <osmair.coelho@gmail.com>
2021-08-17 21:17:01 +0100

    Adding validations

--- app/Http/Livewire/Book/Create.php
+++ app/Http/Livewire/Book/Create.php
@@ -3,11 +3,57 @@
 namespace App\Http\Livewire\Book;
 
 use Livewire\Component;
+use App\Models\Book;
 
 class Create extends Component
 {
+
+    public Book $book;
+    protected $rules = [
+        'book.name' => 'required|string',
+        'book.author' => 'required|integer',
+        'book.pages' => 'required|integer',
+    ];
+
     public function render()
     {
         return view('livewire.book.create');
+    }
+
+    public function mount()
+    {
+        $this->book = new Book();
+    }
+
+
+    /*
+    * validate only one field
+    * */
+   /* public function updatedBookName(){
+        $this->validate();
+    }*/
+
+
+    /*
+     * validate only one field or more
+     * */
+   /* public function updated( $field )
+    {
+        if( $field == 'book.name' )
+        {
+            $this->validade();
+        }
+
+        if( $field == 'book.pages' )
+        {
+            $this->validade();
+        }
+    }*/
+
+    public function save()
+    {
+        $this->validate();
+        book::create($this->book->toArray());
+        return redirect()->route('books.index');
     }
 }
