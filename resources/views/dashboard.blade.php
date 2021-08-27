1bf7381f988d376facfe40fed212959fbac4bcb7
Osmair Coelho <osmair.coelho@gmail.com>
2021-08-27 20:19:19 +0100

    - Button Created when user login to direct to books.index.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="max-w-5xl mx-auto mt-4 mb-4 ml-0 ">
+                        <a href="{{ route('books.index') }}"
+                           class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                            Load books
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
